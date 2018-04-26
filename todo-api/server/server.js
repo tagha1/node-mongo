461c36258667e75ca59e6ae5f5699bd23f049c98
Talha Ghaznavi <talhag@Talhas-MacBook-Pro.local>
2018-04-26 14:49:00 -0500

    added new files and folders

--- todo-api/server/server.js
+++ todo-api/server/server.js
@@ -23,36 +23,4 @@ app.post('/todos', (req, res) => {
 
 app.listen(3500, () => {
   console.log('Localhost started to run localhost:3500')
-})
-
-
-
-// // let newTodo = new Todo({
-// //   text: 'make food'
-// // });
-// //
-// // newTodo.save().then((doc) => {
-// //   console.log('save todo', doc);
-// // }, (err) =>{
-// //   return console.log('Unable to save todo');
-// // });
-//
-// // let addTodo = new Todo({
-// //   text: 'test test'
-// // });
-// //
-// // addTodo.save().then((doc) => {
-// //   console.log(JSON.stringify(doc, undefined, 2));
-// // }, (err) =>{
-// //   return console.log(`Unable to save todo: ${err}`);
-// // });
-//
-// let enterEmail = new User({
-//   email: 'testemail'
-// });
-//
-// enterEmail.save().then((doc) => {
-//   console.log(JSON.stringify(doc, undefined, 2));
-// }, (err) =>{
-//    return console.log(`Unable to save User: ${err}`);
-// });
+});
