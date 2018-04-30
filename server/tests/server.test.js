cc85d2610b84b1f1168bde4b54c5c1cd1c053f47
Talha Ghaznavi <talhag@Talhas-MacBook-Pro.local>
2018-04-30 14:04:43 -0500

    added test case for delete

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -100,3 +100,42 @@ describe('GET /todos/:id', () => {
       .end(done)
   });
 });
+
+describe('Delete /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    let hexId = todos[1]._id.toHexString();
+
+    request(app)
+    .delete(`/todos/${hexId}`)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo._id).toBe(hexId);
+    })
+    .end((err, res) => {
+      if(err) {
+        return done(err);
+      }
+
+      Todo.findById(hexId).then((todo) => {
+        expect(todo).toBeFalsy();
+        done();
+      }).catch((e) => done(e));
+    });
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    let hexId = new ObjectID().toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done)
+  });
+
+  it('should return 404 for non-object ids', (done) => {
+    request(app)
+      .delete('/todos/1234abcd')
+      .expect(404)
+      .end(done)
+  });
+});
