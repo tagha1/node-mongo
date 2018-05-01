7e8317a069c74c6d96bd26dd6e863ace93bc1cb8
Talha Ghaznavi <talhag@Talhas-MacBook-Pro.local>
2018-05-01 11:56:18 -0500

    added new file

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+let env = process.env.NODE_ENV || 'dev';
+
+if (env === 'dev') {
+  process.env.PORT = 3500;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if (env === 'test') {
+  process.env.PORT = 3500;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const Mongoose = require('mongoose');
 
 Mongoose.Promise = global.Promise;
-Mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+Mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = { Mongoose };
