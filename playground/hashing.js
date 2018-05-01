49c2c15b2b4e11738042771675a316de81d1f5c8
Talha Ghaznavi <talhag@Talhas-MacBook-Pro.local>
2018-05-01 16:01:13 -0500

    added hash

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
-  id: 10
-}
+let password = '123abc!';
 
-let token = jwt.sign(data, 'abc1222');
-console.log('token:', token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-let decoded = jwt.verify(token, 'abc1222');
-console.log('decoded', decoded);
+let hashedPassword = '$2a$10$Tp5IOnhxO0f92eqPF4ZbruqtSK76tvXOQbepbpINqy/9pf2aK59o2';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+})
+
+
+// let data = {
+//   id: 10
+// }
+//
+// let token = jwt.sign(data, 'abc1222');
+// console.log('token:', token);
+//
+// let decoded = jwt.verify(token, 'abc1222');
+// console.log('decoded', decoded);
 
 // let message = 'Im a user';
 // let hash = SHA256(message).toString();
