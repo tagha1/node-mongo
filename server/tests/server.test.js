0bc74a8a4f72be059e36b5365cabb97eb5626327
Talha Ghaznavi <talhag@Talhas-MacBook-Pro.local>
2018-05-02 13:42:31 -0500

    added test for DELETE for users.me.token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -173,6 +173,7 @@ describe('PATCH /todos/:id', () => {
   })
 });
 
+//USERS
 describe('GET /user/me', () => {
   it('should return user if authenticated', (done) => {
     request(app)
@@ -294,5 +295,24 @@ describe('POST /users/login', () => {
           done();
         }).catch((e) => done(e));
       });
+  });
+});
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    request(app)
+    .delete('/users/me/token')
+    .set('x-auth', users[0].tokens[0].token)
+    .expect(200)
+    .end((err, res) => {
+      if(err) {
+        return done();
+      }
+
+      User.findById(users[0]._id).then((user) => {
+        expect(user.tokens.length).toBe(0);
+        done();
+      }).catch((e) => done(e));
+    });
   });
 });
